ab0b909e66432bea973ca393a848206ad01cff17
Saúl Hormazábal <saul.hormazabal@gmail.com>
2016-06-13 23:19:17 -0400

    more news componenets

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 
-	grunt.registerTask('default', [ 'watch', 'sass']);
+	grunt.registerTask('default', [ 'sass' , 'watch' ]);
 
 };
